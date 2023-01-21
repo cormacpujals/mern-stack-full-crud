b4807a7541d2101818de43b1744c38a1ecba332d
Cormac Pujals <cmpujals@gmail.com>
2023-01-20 17:45:57 -0800

    database linked

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const db = process.env.DATABASE_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); 
+  }
+};
+
+module.exports = connectDB;
